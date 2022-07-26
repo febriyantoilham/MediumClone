7f0cfbf80bf0f31c29757b22d90cab0969c2d682
febriyantoilham <febriyantoilham@gmail.com>
2022-07-27 02:28:05 +0700

    build

--- medium/components/PostModal.js
+++ medium/components/PostModal.js
@@ -5,7 +5,7 @@ const PostModal = () => {
         /* wrapper */
         <div className="w-[70rem] h-[50rem] flex flex-col justify-start items-center gap-[1rem] p-[1rem] font-mediumSerif overflow-scroll">
         {/* title */}
-            <div className="my-[2rem] font-bold">Create a New Post</div>
+            <div className="my-[2rem] font-bold text-3xl">Create a New Post</div>
         </div>
         
     )
